4f5fe22a8de96d6018e35470631b70981d731477
shson <sunghyun7895@naver.com>
2024-05-08 12:55:43 +0900

    first commit

---
+++ src/style/GlobalStyle.tsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+body {
+    font-weight: 300;
+    font-family: 'Source Sans Pro', sans-serif;
+    color:black;
+    line-height: 1.2;
+    background:linear-gradient(135deg,#e09,#d0e);
+  }
+`;
+
+export default GlobalStyle;
